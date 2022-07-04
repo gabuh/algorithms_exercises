926e7a45f83ba3332a5d26f72699052b0126cdb5
Gabriel <nogueira.santos@aluno.ifsp.edu.br>
2022-07-03 23:15:20 -0300

    chapter7.exA

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # algorithms_exercises
 Exercises from the book Algorithms by Jose Augusto Navarro Garcia Manzano, Jayr Figueiredo de Oliveira
+
+###Chapter 7
+#####[EX.A](/chapter_7/quest0.c)
+![ex.achapter7](/chapter_7/quest0_c7.png)
--- chapter_7/quest0.c
+++ chapter_7/quest0.c
@@ -1,1 +1,38 @@
-a) Elaborar um programa que leia 12 elementos numericos inteiros em uma matriz do tipo vetor. Coloque-os em ordem decrescente e apresente os elementos ordenados.
+/*a) Elaborar um programa que leia 12 elementos numericos inteiros em uma matriz do tipo vetor. Coloque-os em ordem decrescente e apresente os elementos ordenados.
+*/
+
+
+#include<stdio.h>
+#define TAM 12
+
+int main(){
+ int vetor[TAM]={0}; //vetor inteiro
+ int temp;   //variavel temporaria.
+
+for (int i=0; i<TAM;++i){
+  scanf("%d",&vetor[i]);
+  for (int ii=0;ii<i;++ii){
+    printf("%d ",vetor[ii]);
+  }
+  printf(">");
+}
+//Ordenando de forma decrescente.
+for (int i=0; i<TAM;++i){
+    for (int ii=0;ii<TAM;++ii){
+      temp=vetor[i];
+      if(vetor[ii]<vetor[i]){
+        vetor[i]=vetor[ii];
+        vetor[ii]=temp;
+      }
+    }
+}
+
+//apresentando os elemetos.
+  printf("\nResultado:\n");
+for(int i=0; i<TAM;++i){
+  printf("%d ",vetor[i]);
+}
+
+  return 0;
+  
+}
