918842de7ae8e5c095da1333079ddea3cbad24b5
Gabriel <nogueira.santos@aluno.ifsp.edu.br>
2022-07-06 14:25:39 -0300

    Ex. C chapter 7

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Exercises from the book Algorithms by Jose Augusto Navarro Garcia Manzano, Jayr 
 ### Chapter 7
 ##### [EX.A](/chapter_7/quest0.c)
 ![ex.achapter7](/chapter_7/quest0_c7.png)
+
+##### [Ex.B](/chapter_7/quest1.c)
+
+##### [Ex.C](/chapter_7/quest2.c)
+![ex.cchapter7](/chapter_7/quest2_c7.png)
--- chapter_7/quest2.c
+++ chapter_7/quest2.c
@@ -1,1 +1,53 @@
+/*
 c) Construir um programa que leia 15 elementos numericos inteiros em uma matriz A de uma dimensao do tipo vetor. Construir uma matriz B de mesmo tipo e dimensao, em que cada elemento seja o fatorial do elemento correspondente armazenado na matriz A. Apresentar os elementos da matriz B ordenados de forma crescente.
+*/
+
+#include<stdio.h>
+#define TAM 15
+
+int fatorial(int n){
+  int f,i;
+  f=1;
+  for(i=1; i <=n ; i++){
+    f=f*i;
+  }
+return f;
+}
+
+
+int main(){
+int A[TAM];  
+int B[TAM];  
+int temp;
+
+for (int i=0;i<TAM;++i){
+  scanf("%d",&A[i]);
+  for (int ii=0;ii<=i;ii++){
+    printf("%d ",A[ii]);
+  }
+printf(">");
+}  
+//farotial + matriz b 
+for (int i=0; i<TAM;++i){
+  B[i]=fatorial(A[i]);
+}
+printf("\n...fatorando...\n");
+
+for (int i=0; i<TAM;++i){
+  temp=B[i];
+  for (int ii=0; ii<TAM;++ii){
+    if (B[ii]>B[i]){
+      B[i]=B[ii];
+      B[ii]=temp;
+      temp=B[i];
+    }
+  }
+}
+
+printf("\n Vetor 'B' ordenado de forma crescente: ");
+for(int i=0; i<TAM;i++){
+  printf("%d ",B[i]);
+}
+  
+  return 0;
+}
