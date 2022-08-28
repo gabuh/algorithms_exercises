a6767aa788f47b6b727e9d54328bd445443bf4b0
Gabriel <nogueira.santos@aluno.ifsp.edu.br>
2022-08-28 11:20:46 -0300

    r_c7

--- ch7/r.c
+++ ch7/r.c
@@ -3,11 +3,11 @@ r) Elaborar um programa que efetue a leitura de dados em duas matrizes (A e B) d
 */
 #include<stdio.h>
 #include<string.h>
-#define TAM 6
+#define TAM 10
 
-int searchIndex(char compChar){
-  char abc[]={"abcdefghijklmnopqrstuvwxyz"};
-  for (int i=0;i<26;i++){
+int searchSize(char compChar){
+  char abc[]={"'` ,[]{}.?~!@#$%^*=+-AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"};
+  for (int i=0;i<74;i++){
       if(abc[i]==compChar){
         return i;
       }
@@ -20,49 +20,46 @@ struct String{
 };
 
 int main(){
-  char value[100];
+  char temp[100];
   struct String A[TAM];
   struct String B[TAM-5];
   struct String C[TAM+5];
-  
 
-  
-  
-printf("Complete A:\n");
+printf("Complete A writing something:\n");
   for(int i=0;i<TAM;i++){
-    fgets(value,100,stdin);
-    strcpy(A[i].value,value);
+    printf("%d : ",i+1);
+    fgets(temp,100,stdin);
+    strcpy(A[i].value,temp);
   }
-printf("Complete B:\n");
+printf("Complete B writing something:\n");
   for(int i=0;i<TAM-5;i++){
-    fgets(value,100,stdin);
-    strcpy(B[i].value,value);
+    printf("%d : ",i+1);
+    fgets(temp,100,stdin);
+    strcpy(B[i].value,temp);
   }
   
 for(int i=0;i<(2*TAM)-5;i++){
-    printf("%d",i);
   if(i<TAM-5){
-    // printf("%s",B[i].value);
     strcpy(C[i].value,B[i].value);
   }else if(i<TAM+5){
-    // printf("%s",A[i-(TAM-5)].value);
     strcpy(C[i].value,A[i-(TAM-5)].value);
   }
 }
 
+for(int i=0;i<((TAM*2)-5)-1;++i){
+  for(int j=0;j<((TAM*2)-5)-1-i;++j){
+        if(searchSize(C[j].value[0])<searchSize(C[j+1].value[0])){
+          strcpy(temp,C[j].value);
+          strcpy(C[j].value,C[j+1].value);
+          strcpy(C[j+1].value,temp);
+        }
+  }
+}
 
-
-
-// printf("Here is C:\n");
-// for(int i=0;i<TAM+5;i++){
-//   printf("%s",C[i].value);
-// }
-
-printf("\ndone\n");
-
-
-
-
-
+printf("Here is descending C:\n");
+for(int i=0;i<(TAM*2)-5;++i){
+  printf("%d : ",i+1);
+  printf("%s",C[i].value);
+}
   return 0;
 }
