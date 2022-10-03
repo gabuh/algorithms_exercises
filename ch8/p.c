2a05028b431dc367c3c8c76b8ad2e8329d222ac5
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2022-10-02 23:53:01 -0300

    p_c8

--- ch8/p.c
+++ ch8/p.c
@@ -1,1 +1,50 @@
-p) Elaborar um programa que leia uma matriz A de duas dimensoes com dez linhas e sete colunas. Ao final apresentar o total de elementos pares e impares existentes na matriz. Apresentar tambem o percentual de elementos pares e impares em relaçao ao total de elementos da matriz. Supondo a existência de 20 elementos pares e 50 elementos impares, ter-se-ia 28,6% de elementos pares e 71,4% de elementos impares.
+/*
+p) Elaborar um programa que leia uma matriz A de duas dimensoes com dez linhas e sete colunas.
+Ao final apresentar o total de elementos pares e impares existentes na matriz.
+Apresentar tambem o percentual de elementos pares e impares em relaï¿½ao ao total de elementos da matriz.
+Supondo a existï¿½ncia de 20 elementos pares e 50 elementos impares, ter-se-ia 28,6% de elementos pares e 71,4% de elementos impares.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#define lin 10
+#define col 7
+
+
+int main(){
+    int A[lin][col];
+    int odd,even;
+    odd = even = 0;
+
+    for(int i=0;i<lin;i++){
+        for (int j=0;j<col;j++){
+            A[i][j]=rand()%10;
+        }
+    }
+
+
+    for(int i=0;i<lin;i++){
+        for (int j=0;j<col;j++){
+            if(A[i][j]%2==0){
+                even++;
+            }else{
+                odd++;
+            }
+
+        }
+    }
+
+
+    for(int i=0;i<lin;i++){
+        for (int j=0;j<col;j++){
+            printf("%d ",A[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+    printf("Odd Total: %d, %f%%\nEven Total: %d, %f%%\n",odd,(float)((double)100/(lin*col))*odd,even,(float)((double)100/(lin*col))*even);
+
+    return 0;
+}
