2c56683a561438a45da5258c61e4fd8638cae2c0
Gabriel <nogueira.santos@aluno.ifsp.edu.br>
2022-10-24 09:50:54 -0300

    e_c6

--- ch6/e.c
+++ ch6/e.c
@@ -1,1 +1,33 @@
-e) Elaborar um programa que leia uma matriz A do tipo vetor com 15 elementos inteiros. Construir uma matriz B de mesmo tipo, e cada elemento da matriz B deve ser o resultado da fatorial correspondente de cada elemento da matriz A. Apresentar as matrizes A e B.
+/*e) Elaborar um programa que leia uma matriz A do tipo vetor com 15 elementos inteiros. Construir uma matriz B de mesmo tipo, e cada elemento da matriz B deve ser o resultado da fatorial correspondente de cada elemento da matriz A. Apresentar as matrizes A e B.
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#define TAM 15
+
+int factorial(n){
+  int f=1;
+  for(int i=1;i<=n;i++){
+	f=f*i;
+  }
+
+return f;
+}
+
+
+int main(){
+  int A[TAM];
+  int B[TAM];
+
+  for(int i=0; i<TAM; i++){
+	  A[i] = rand()%10;
+	  B[i] = factorial(A[i]);
+  	  printf("%d! = %d \n",A[i],B[i]);
+  }
+  
+
+
+
+
+
+return 0;
+}
