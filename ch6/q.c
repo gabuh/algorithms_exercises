9ce487f2123a6618742047d1dcf628910c0daa02
Gabriel <nogueira.santos@aluno.ifsp.edu.br>
2022-11-01 05:55:55 -0300

    q_c6

--- ch6/q.c
+++ ch6/q.c
@@ -1,1 +1,25 @@
-q) Elaborar um programa que leia 15 elementos reais para uma matriz A de uma dimensao do tipo vetor. Construir uma matriz B de mesmo tipo e dimensao, observando a seguinte lei de formaçao: "todo elemento da matriz A que possuir indice par deve ter seu elemento dividido por 2; caso contrario, o elemento da matriz A deve ser multiplicado por 1.5". Apresentar os elementos da matriz B.
+/*q) Elaborar um programa que leia 15 elementos reais para uma matriz A de uma dimensao do tipo vetor. Construir uma matriz B de mesmo tipo e dimensao, observando a seguinte lei de formaçao: "todo elemento da matriz A que possuir indice par deve ter seu elemento dividido por 2; caso contrario, o elemento da matriz A deve ser multiplicado por 1.5". Apresentar os elementos da matriz B.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TAM 15
+
+int main(){
+  float A[TAM];
+  float B[TAM];
+
+  for(int i=0 ;i<TAM; i++){
+	A[i] = rand()%10;
+	if(i%2==0){
+		B[i] = A[i] / 2;
+		printf("Index -> %d | %f /2 = %f\n",i,A[i],B[i]);
+	}else{
+		B[i] = A[i] * 1.5;
+		printf("Index -> %d | %f * 1.5 = %f\n",i,A[i], B[i]);
+	}
+  }
+
+
+return 0;
+}
