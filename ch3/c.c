69cccf4c6eda5601036e5b0d2bb4bb292b455534
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-03-19 00:25:03 -0300

    ch3 c

--- ch3/c.c
+++ ch3/c.c
@@ -1,2 +1,26 @@
-c) Calcular e apresentar o valor do volume de uma lata de oleo, utilizando a formula:
-Volume Raio ** Altura 2 <-- PI.
+// c) Calcular e apresentar o valor do volume de uma lata de oleo, utilizando a formula:
+// Volume Raio ** Altura 2 <-- PI.
+
+
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+void main(){
+    float volume;
+    float raio;
+    float altura;
+    
+    printf("Raio: ");
+    scanf("%f",&raio);
+
+    printf("\nAltura: ");
+    scanf("%f",&altura);
+
+    volume = PI * pow(altura,2) * raio; 
+    
+    printf("\n volume: %f",volume);
+
+
+}
