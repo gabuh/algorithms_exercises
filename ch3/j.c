6afc8ddd110b7dece5075f7ebc081d341d2f4fb1
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-04-08 07:58:25 -0300

    ch3 -> j

--- ch3/j.c
+++ ch3/j.c
@@ -1,1 +1,20 @@
-j) Ler dois inteiros (variaveis A e B) e imprimir o resultado do quadrado da diferença do primeiro valor pelo segundo.
+/*
+j) Ler dois inteiros (variaveis A e B) e imprimir o resultado do quadrado da diferenï¿½a do primeiro valor pelo segundo.
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int A, B;
+
+    printf("First Value: ");
+    scanf("%d",&A);
+
+    printf("Second Value: ");
+    scanf("%d",&B);
+
+    printf("First Value ^ 2 - Second Value ^ 2 = %f", pow((double)A,2) - pow((double)B,2));
+
+    return 0;
+}
