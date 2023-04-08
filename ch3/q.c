24ef1b6780e5036d98acf5b35749dd63a56817e3
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-04-08 09:18:32 -0300

    ch3 q

--- ch3/q.c
+++ ch3/q.c
@@ -1,1 +1,19 @@
-q) Elaborar um programa que calcule e apresente o valor do resultado da area de uma circunferência (variavel A). O programa deve solicitar a entrada do valor do raio da circunferência (variavel R). Para a execuçao deste problema utilize a formula A-3.14159265* R ^ 2.
+/*
+q) Elaborar um programa que calcule e apresente o valor do resultado da area de uma circunferï¿½ncia (variavel A). O programa deve solicitar a entrada do valor do raio da circunferï¿½ncia (variavel R). Para a execuï¿½ao deste problema utilize a formula A -> 3.14159265* R ^ 2.
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    float R;
+
+    printf("Digite Circuferencia: ");
+    scanf("%f",&R);
+
+
+    printf("Area is: %f sqh", 3.14159265 * pow((double) R,2));
+
+
+    return 0;
+}
