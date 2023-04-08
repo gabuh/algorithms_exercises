1740de77987712eaff5c3265275720e7bfdefe55
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-04-08 08:15:22 -0300

    ch3 -> l

--- ch3/l.c
+++ ch3/l.c
@@ -1,1 +1,22 @@
-l) Elaborar um programa que efetue a apresentaçao do valor da conversao em dolar de um valor lido em real. O programa deve solicitar o valor da cotaçao do dolar e tambem a quantidade de reais disponivel com o usuario, para que seja apresentado o valor em moeda americana.
+/*
+l) Elaborar um programa que efetue a apresentaï¿½ao do valor da conversao em dolar de um valor lido em real. O programa deve solicitar o valor da cotaï¿½ao do dolar e tambem a quantidade de reais disponivel com o usuario, para que seja apresentado o valor em moeda americana.
+*/
+
+#include<stdio.h>
+
+int main(){
+
+float cotacao;
+float quantidadeMoney;
+
+    printf("Cotacao do dolar hoje: R$ ");
+    scanf("%f",&cotacao);
+
+    printf("\nTeu amount em real: R$ ");
+    scanf("%f",&quantidadeMoney);
+
+    printf("\nValor convertido to dollar: $ %f ",quantidadeMoney / cotacao);
+    
+
+    return 0;
+}
