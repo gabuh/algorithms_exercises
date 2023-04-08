b6e36ef0b58e9f16d84976860f8daf3074634128
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-04-08 13:54:07 -0300

    ch -> r

--- ch3/r.c
+++ ch3/r.c
@@ -1,1 +1,115 @@
-r) Em uma eleiçao sindical concorreram ao cargo de presidente três candidatos (representados pelas variaveis A, B e C). Durante a apuraçao dos votos foram computados votos nulos e em branco, alem dos votos validos para cada candidato. Deve ser criado um programa de computador que faça a leitura da quantidade de votos validos para cada candidato, alem ler tambem a quantidade de votos nulos e em branco. Ao final o programa deve apresentar o numero total de eleitores, considerando votos validos, nulos e em branco; o percentual correspondente de votos validos em relaçao à quantidade de eleitores; o percentual correspondente de votos validos do candidato A em relaçao à quantidade de eleitores; o percentual correspondente de votos valido do candidato B em relaçao à quantidade de eleitores; o percentual correspondente de votos validos do candidato C em relaçao quantidade de eleitores; o percentual correspondente de votos nulos em relaçao à quantidade de eleitores; e por ultimo o percentual correspondente de votos em branco em relaçao à quantidade de eleitores.
+/*
+r) Em uma eleiï¿½ao sindical concorreram ao cargo de presidente trï¿½s candidatos (representados pelas variaveis A, B e C). Durante a apuraï¿½ao dos votos foram computados votos nulos e em branco, alem dos votos validos para cada candidato. Deve ser criado um programa de computador que faï¿½a a leitura da quantidade de votos validos para cada candidato, alem ler tambem a quantidade de votos nulos e em branco. Ao final o programa deve apresentar o numero total de eleitores, considerando votos validos, nulos e em branco; o percentual correspondente de votos validos em relaï¿½ao ï¿½ quantidade de eleitores; o percentual correspondente de votos validos do candidato A em relaï¿½ao ï¿½ quantidade de eleitores; o percentual correspondente de votos valido do candidato B em relaï¿½ao ï¿½ quantidade de eleitores; o percentual correspondente de votos validos do candidato C em relaï¿½ao quantidade de eleitores; o percentual correspondente de votos nulos em relaï¿½ao ï¿½ quantidade de eleitores; e por ultimo o percentual correspondente de votos em branco em relaï¿½ao ï¿½ quantidade de eleitores.
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+struct Candidato
+{   
+    char nome;
+    int quantidade;
+    int voto[30];
+};
+
+void addVoto(struct Candidato* a, int tipoDeVoto){
+    a->voto[a->quantidade] = tipoDeVoto;
+    a->quantidade++;
+    printf("\n%c --> %d , quant. votos: %d",a->nome ,tipoDeVoto , a->quantidade);
+}
+
+void printAll(struct Candidato* a){
+    printf("\n\nPRINTING-----------CANDIDATO: %c\n",a->nome);
+    for(int i=0; i < a->quantidade; i++){
+        if(a->voto[i] == 1 ){
+            printf("%d Voto Valido -> %d\n",a->voto[i],i);
+        } else if (a->voto[i] == 2 ){
+            printf("%d Voto Branco -> %d\n",a->voto[i],i);
+        } else if (a->voto[i] == 3 ){
+            printf("%d Voto Nulo -> %d\n",a->voto[i],i);
+        }
+    }
+}
+
+
+int quantidadeVotosValidos(struct Candidato* a){
+    int votos=0;
+    for(int i=0; i < a->quantidade; i++){
+       if(a->voto[i]==1){
+        votos++;
+       }
+    }
+    return votos;
+}
+
+int quantidadeVotosNulos(struct Candidato* a){
+    int votos=0;
+    for(int i=0; i < a->quantidade; i++){
+       if(a->voto[i]==3){
+        votos++;
+       }
+    }
+    return votos;
+}
+
+int quantidadeVotosBrancos(struct Candidato* a){
+    int votos=0;
+    for(int i=0; i < a->quantidade; i++){
+       if(a->voto[i]==2){
+        votos++;
+       }
+    }
+    return votos;
+}
+
+void porcentagemDeVotos(struct Candidato* a, int quantidadeTotalDeEleitores){
+        printf("\n\nPorcentagem De Votos-----------CANDIDATO: %c",a->nome);
+        printf("\nVoto Valido ->: %f ",(double) (quantidadeVotosValidos(a) * 100) / quantidadeTotalDeEleitores);
+        printf("\nVoto Branco ->: %f ",(double) (quantidadeVotosBrancos(a) * 100) / quantidadeTotalDeEleitores);
+        printf("\nVoto Nulo ->: %f ",(double) (quantidadeVotosNulos(a) * 100) / quantidadeTotalDeEleitores);
+}
+
+
+int main(){ 
+    struct Candidato A;
+    struct Candidato B;
+    struct Candidato C;
+
+    A.quantidade = 0;
+    A.nome = 'A';
+    B.quantidade = 0;
+    B.nome = 'B';
+    C.quantidade = 0;
+    C.nome = 'C';
+    
+    addVoto(&A,2);
+    addVoto(&A,1);
+    addVoto(&A,1);
+    addVoto(&A,1);
+    addVoto(&A,1);
+
+    addVoto(&B,1);
+    addVoto(&B,1);
+    addVoto(&B,3);
+    addVoto(&B,1);
+    addVoto(&B,1);
+
+    addVoto(&C,1);
+    addVoto(&C,1);
+    addVoto(&C,1);
+    addVoto(&C,2);
+    addVoto(&C,1);
+    addVoto(&C,1);
+
+    printAll(&A);
+    printAll(&B);
+    printAll(&C);
+
+
+    porcentagemDeVotos(&A, A.quantidade+B.quantidade+C.quantidade);
+    porcentagemDeVotos(&B, A.quantidade+B.quantidade+C.quantidade);
+    porcentagemDeVotos(&C, A.quantidade+B.quantidade+C.quantidade);
+
+
+    return 0;
+}
