4251fd3f24de9850e9befa3f167b222b84868941
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-04-08 08:55:52 -0300

    ch3 -> p

--- ch3/p.c
+++ ch3/p.c
@@ -1,1 +1,23 @@
-p) Elaborar um programa que leia o valor numerico correspondente ao salario mensal (variavel SM) de um trabalhador e tambem faça a leitura do valor do percentual de reajuste (variavel PR) a ser atribuido. Apresentar o valor do novo salario (variavel NS).
+/*
+p) Elaborar um programa que leia o valor numerico correspondente ao salario mensal (variavel SM) de um trabalhador e tambem faï¿½a a leitura do valor do percentual de reajuste (variavel PR) a ser atribuido. Apresentar o valor do novo salario (variavel NS).
+*/
+
+
+#include<stdio.h>
+
+int main(){
+    float income;
+
+    float adjust;
+
+    printf("Type ur income $: ");
+    scanf("%f",&income);
+    printf("\nType the percent adjust: %%");
+    scanf("%f",&adjust);
+
+    
+    printf("\nType the adjust of %d%% is: %f ", (int) adjust,((income/100) * adjust) + income);
+
+
+    return 0;
+}
